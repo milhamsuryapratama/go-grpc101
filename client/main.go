352760388f486d1caad085da18da9d6361cb7081
milhamsuryapratama <ilhamsurya26@gmail.com>
2025-07-22 09:11:32 +0700

    ini service

---
+++ client/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	pbHello "grpc-101/grpc/hello"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Connected to server")
+
+	s := pbHello.NewUserServiceClient(conn)
+	resp, err := s.CreateUser(context.Background(), &pbHello.CreateUserRequest{
+		User: &pbHello.User{
+			Name:  "Ilham",
+			Email: "ilham@gmail.com",
+		},
+	})
+
+	if err != nil {
+		log.Fatalf("could not create user: %v", err)
+	}
+
+	log.Println("User created successfully", resp.User.Name, resp.User.Email)
+
+	// resp, err := s.SayHello(context.Background(), &pbHello.HelloRequest{Name: "Ilham"})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// log.Printf("Greeting: %s", resp.Message)
+
+	// gb, err := s.SayGoodbye(context.Background(), &pbHello.HelloRequest{Name: "Kay"})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// log.Printf("Greeting: %s", gb.Message)
+}
